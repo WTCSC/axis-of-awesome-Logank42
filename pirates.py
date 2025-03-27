fb7239819bd5a827e214b81403db2b607fd895b3
Logan <logan_kirkpatrick@icloud.com>
2025-03-27 15:14:08 -0600

    Finished pt2

--- pirates.py
+++ pirates.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Data
 pirate_attack_data = {
     "Number of Pirate Attacks": [445, 439, 297, 264, 245, 246, 191, 180, 201, 162, 195, 132, 115, 120, 116],
     "Year": ["2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023", "2024"]
@@ -8,16 +9,19 @@ pirate_attack_data = {
 
 df = pd.DataFrame(pirate_attack_data)
 
-plt.figure(figsize=(15, 9.5))
-plt.bar(df["Year"], df["Number of Pirate Attacks"], color="Navy")
+plt.figure(figsize=(15, 9.5)) # Graph size
+plt.bar(df["Year"], df["Number of Pirate Attacks"], color="Navy") # Bar graph
+# Labels
 plt.xlabel("Year")
 plt.ylabel("Number of Pirate Attacks")
 plt.title("Number of Pirate Attacks Each year (2010-2024)")
 
-plt.figure(figsize=(15, 9.5))
-plt.plot(df["Year"], df["Number of Pirate Attacks"], color="Navy")
+plt.figure(figsize=(15, 9.5)) # Graph size
+plt.plot(df["Year"], df["Number of Pirate Attacks"], color="Navy") # Line graph
+# Labels
 plt.xlabel("Year")
 plt.ylabel("Number of Pirate Attacks")
 plt.title("Number of Pirate Attacks Each year (2010-2024)")
 
+# Shows the graphs
 plt.show()
